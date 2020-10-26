871d7987fb98d61181d51419c81403262606678b
Rama Ibrahim <maulanarama@gmail.com>
2020-10-26 21:17:19 +0700

    rest api release done

--- server/controllers/TodoController.js
+++ server/controllers/TodoController.js
@@ -50,5 +50,71 @@ class Controller {
             res.status(404).json(error)
         }
     }
+
+    static async update(req, res){
+        try {
+            const payload = {
+                title: req.body.title,
+                description: req.body.description,
+                status: req.body.status,
+                due_date: new Date(req.body.due_date)
+            
+            }
+            let todo = await Todo.update(payload, {where: {id: req.params.id}, returning:true })
+            res.status(200).json({
+                id: todo[1][0].id,
+                title: todo[1][0].title,
+                description: todo[1][0].description,
+                status: todo[1][0].status,
+                due_date: new Date(todo[1][0].dues_date)
+            });
+        } catch (error) {
+            if(error.name === "SequelizeValidationError"){
+                res.status(400).json({error: error.message})
+            } else if(error.name === "TypeError") {
+                res.status(404).json({error:"Not Found"})
+            } else {
+                res.status(500).json({error})
+            }
+        }
+    }
+
+    static async patchTodo(req, res){
+        try {
+
+            const payload = {
+                status: req.body.status
+            }
+            let todo = await Todo.update(payload, { where: {id: req.params.id}, returning:true });
+            res.status(200).json({
+                id: todo[1][0].id,
+                title: todo[1][0].title,
+                description: todo[1][0].description,
+                status: todo[1][0].status,
+                due_date: new Date(todo[1][0].dues_date)
+            });
+        } catch (error){
+            if(error.name === "SequelizeValidationError"){
+                res.status(400).json({error: error.message})
+            } else if(error.name === "TypeError") {
+                res.status(404).json({error:"Not Found"})
+            } else {
+                res.status(500).json({error})
+            }
+        }
+    }
+
+    static async delete(req, res){
+        try {
+            let todo = await Todo.destroy({where:{id: req.params.id}})
+            res.status(200).json({message: "todo success to delete"})
+        } catch (error){
+            if(error.name === "TypeError") {
+                res.status(404).json({error:"Not Found"})
+            } else {
+                res.status(500).json({error: error.message})
+            }
+        }
+    }
 }
 module.exports = Controller
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,10 @@ router.post('/todos', Controller.create)
 router.get('/todos', Controller.showTodos)
 
 router.get('/todos/:id', Controller.showTodo)
+router.put('/todos/:id', Controller.update)
+router.patch('/todos/:id', Controller.patchTodo)
 
+router.delete('/todos/:id', Controller.delete)
 
 
 module.exports = router
