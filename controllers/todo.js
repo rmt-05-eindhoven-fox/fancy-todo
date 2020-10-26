de2c381406b6a1dc963d421802f47a2dfa5bacf4
Nikolas Stefano <nz.stefano@gmail.com>
2020-10-26 23:50:30 +0700

    Rest API Todo

--- controllers/todo.js
+++ controllers/todo.js
@@ -2,21 +2,107 @@ const { Todo } = require("../models")
 
 class TodoController {
 
-    static async test (req, res) {
+    static async showTodos (req, res) {
         try {
-            res.send(`test`)
+            const userId = null
+            const todos = await Todo.findAll({
+                where: {
+                    UserId: userId
+                }
+            })
+            res.status(200).json({
+                todos
+            })
         } catch(err) {
-
+            res.send(500).json({err})
         }
     }
 
-    static showAllTodos (req, res) {
-        const userId = null
-        Todo.findAll({
-            where: {
-                id: userId
+    static async createTodo (req, res) {
+        try {
+            const newTodo = {
+                title: req.body.title,
+                description: req.body.description,
+                due_date: req.body.due_date
             }
-        })
+            const todo = await Todo.create(newTodo)
+            res.status(201).json({todo})
+        } catch(err) {
+            if (err.name === "SequelizeValidationError") {
+                res.status(401).json(err)
+            } else {
+                res.status(500).json(err)
+            }
+        }
+    }
+
+    static async updateTodo (req, res) {
+        try {
+            const targetId = req.params.id
+            const newData = {
+                title: req.body.title,
+                description: req.body.description,
+                status: req.body.status,
+                due_date: req.body.due_date
+            }
+
+            const todo = await Todo.update(newData, {
+                where: {id: targetId}
+            })
+            res.status(200).json({todo})
+
+        } catch(err) {
+            if (err.name === "SequelizeValidationError") {
+                res.status(400).json({err})
+            } else if (err.name === "") {
+                res.status(404).json({err})
+            } else {
+                res.status(500).json(err)
+            }
+        }
+    }
+
+    static async updateTodoStatus (req, res) {
+        try {
+            const targetId = req.params.id
+            const newData = {
+                status: req.body.status,
+            }
+
+            const todo = await Todo.update(newData, {
+                where: {id: targetId}
+            })
+            res.status(200).json({todo})
+
+        } catch(err) {
+            if (err.name === "SequelizeValidationError") {
+                res.status(400).json({err})
+            } else if (err.name === "") {
+                res.status(404).json({err})
+            } else {
+                res.status(500).json(err)
+            }
+        }
+    }
+
+    static async deleteTodo (req, res) {
+        try {
+            const targetId = req.params.id
+
+            const todo = await Todo.destroy({
+                where: {id: targetId}
+            })
+            if (todo === 0) {
+                throw new Error(`Data not found`)
+            }
+            res.status(200).json({message: `${todo} success to delete`})
+        } catch(err) {
+            if (err.message === "Data not found") {
+                res.status(404).json(err.message)
+            } else {
+                res.status(500).json(err.message)
+            }
+        }
     }
 
 }
