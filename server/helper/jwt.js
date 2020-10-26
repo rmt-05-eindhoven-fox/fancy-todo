6b36fdd068fd2837d707783a710788754a3c87dc
rpratama94 <rpratama.sky@gmail.com>
2020-10-26 22:13:51 +0700

    Add User Model and Implement JWT

--- server/helper/jwt.js
+++ server/helper/jwt.js
@@ -1,9 +1,7 @@
 var jwt = require('jsonwebtoken');
 
 function generateToken(payloads) {
-  const token = jwt.sign(JSON.stringify(payloads), process.env.PRIVATE_KEY, {
-    expiresIn: 86400 // token kadaluwarsa selama 24 jam
- });
+  const token = jwt.sign(JSON.stringify(payloads), process.env.PRIVATE_KEY);
   return token;
 }
 
