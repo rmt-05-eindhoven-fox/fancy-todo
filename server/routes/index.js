5a7e526f1d5f65142f5be4a602ab113f39d142a6
Dicky Andeyan <dickyundead@gmail.com>
2020-10-26 19:37:42 +0700

    create model and migration etc

---
+++ server/routes/index.js
@@ -0,0 +1,15 @@
+const router = require('express').Router()
+
+
+// router.get('/todos', Controller.getTodo)
+// router.post('/todos', Controller.postTodo)
+
+// router.post('/register', Controller.postRegister)
+// router.post('/register', Controller.postLogin)
+
+// router.get('/todos/:id', Controller.getTodoById)
+// router.put('/todos/:id', Controller.updateToDo)
+// router.patch('/todos/:id', Controller.statusTodo)
+// router.delete('/todos/:id', Controller.deleteTodo)
+
+module.exports = router
