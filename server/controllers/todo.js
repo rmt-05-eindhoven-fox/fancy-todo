336f5affbc3947260f8fb6ea19f73ce49e3baa2e
athunkadi <caturadi.nu@gmail.com>
2020-10-27 08:34:38 +0700

    done dokumentasi and crud

--- server/controllers/todo.js
+++ server/controllers/todo.js
@@ -2,69 +2,104 @@ const { Todo } = require('../models');
 
 class TodoController {
 
-   static findAll(req, res){
-    Todo.findAll()
-      .then(data => [
-        res.status(201).json(data)
-      ])
-      .catch(err => {
-        res.status(500).json(err)
-      })
-   }
+  static findAll(req, res){
+  Todo.findAll()
+    .then(data => [
+      res.status(201).json(data)
+    ])
+    .catch(err => {
+      res.status(500).json(err)
+    })
+  }
 
-   static create(req, res){
-    let obj = {
-      title: req.body.title,
-      description: req.body.description,
-      due_date: req.body.due_date,
-      status: req.body.status
-    }
-    Todo.create(obj)
-      .then(data => {
+  static create(req, res){
+  let obj = {
+    title: req.body.title,
+    description: req.body.description,
+    due_date: req.body.due_date,
+    status: req.body.status
+  }
+  Todo.create(obj)
+    .then(data => {
+      res.status(201).json(data);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+  }
+
+  static findId(req, res){
+  const id = +req.params.id;
+
+  Todo.findByPk(id)
+    .then(data => {
+      if(data){
         res.status(201).json(data);
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      })
-   }
+      } else {
+        throw res.status(404).json({error : "not found"});
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+  }
 
-   static findId(req, res){
+  static update(req, res){
+  const id = +req.params.id;
+  const obj = {
+    title: req.body.title,
+    description: req.body.description,
+    due_date: req.body.due_date,
+    status: req.body.status
+  }
+
+  Todo.update(obj, { where : {'id' : id}, returning: true})
+    .then(data => {
+      if(data[1][0]){
+        res.status(201).json(data[1][0]);
+      } else {
+        throw res.status(404).json({error : "not found"});
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+  }
+
+  static updateStatus(req, res){
+  const id = +req.params.id;
+  const obj = {
+    status: req.body.status
+  }
+
+  Todo.update(obj, { where : {'id' : id}, returning: true})
+    .then(data => {
+      if(data[1][0]){
+        res.status(201).json(data[1][0]);
+      } else {
+        throw res.status(404).json({error : "not found"});
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+  }
+
+  static delete(req, res){
     const id = +req.params.id;
 
-    Todo.findOne({where : {'id' : id }, returning: true})
+    Todo.destroy({where : {'id': id}})
       .then(data => {
         if(data){
-          res.status(201).json(data);
+          res.status(201).json({message : 'todo success to delete'})
         } else {
-          throw res.status(404).json(data);
+          res.status(404).json({error : "not found"});
         }
       })
       .catch(err => {
         res.status(500).json(err);
       })
-   }
-
-   static update(req, res){
-    const id = +req.params.id;
-    const obj = {
-      title: req.body.title,
-      description: req.body.description,
-      due_date: req.body.due_date,
-      status: req.body.status
-    }
-
-    Todo.update(obj, { where : {'id' : id}, returning: true})
-      .then(data => {
-        if(data){
-          res.status(201).json(data[1][0]);
-        } else {
-          throw res.status(404).json(data);
-        }
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      })
-   }
+  }
 
 }
 
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -6,6 +6,8 @@ todoRouter.get('/todos',TodoController.findAll);
 todoRouter.post('/todos', TodoController.create);
 todoRouter.get('/todos/:id', TodoController.findId);
 todoRouter.put('/todos/:id', TodoController.update);
+todoRouter.patch('/todos/:id', TodoController.updateStatus);
+todoRouter.delete('/todos/:id', TodoController.delete);
 
 
 module.exports = todoRouter
