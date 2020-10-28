a90de2bd33854ece7449ee141aa86142a0027643
dickyandeyan <dickyundead@gmail.com>
2020-10-29 00:46:06 +0700

    Add login page & empty homepage

--- server/app.js
+++ server/app.js
@@ -1,15 +1,20 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT
 const router = require('./routes/index')
 const errorHandler = require('./middlewares/errorHandler')
 
+
+app.use(cors())
 app.use(express.urlencoded({
   extended: false
 }))
 app.use(express.json())
+
+
 app.use(router)
 app.use(errorHandler)
 
