f17915c64bcb48b09d4e1018ed6d6602df372425
dewijoanne <44321548+dewijoanne@users.noreply.github.com>
2020-10-29 16:10:36 +0700

    Update main.js

--- client/main.js
+++ client/main.js
@@ -16,14 +16,14 @@ function register(event) {
 
   $.ajax({
     url: `${baseUrl}/register`,
-    method: "post",
+    method: "POST",
     data: {
       email,
       password
     }
   })
   .done(data => {
-    showSuccessToastMessage('Your account has been created')
+    showSuccessToastMessage('Yes! Account has been created!')
     goToLogin()
   })
   .fail(err => {
@@ -38,7 +38,7 @@ function login(event) {
 
   $.ajax({
     url: `${baseUrl}/login`,
-    method: "post",
+    method: "POST",
     data: {
       email,
       password
@@ -46,7 +46,7 @@ function login(event) {
   })
   .done(data => {
     localStorage.setItem("token", data.token)
-    showSuccessToastMessage('Login successful')
+    showSuccessToastMessage('Welcome!')
     checkLogin()
   })
   .fail(err => {
@@ -93,6 +93,24 @@ function goToRegister() {
 }
 
 
+function onSignIn(googleUser) {
+  var google_access_token = googleUser.getAuthResponse().id_token;
+
+  $.ajax({
+    url: baseUrl + '/googleLogin',
+    method: 'POST',
+    data: {
+      google_access_token
+    }
+  })
+  .done(data => {
+    localStorage.setItem("token", data.token)
+    checkLogin()
+  })
+  .fail(err => {
+    console.log(err)
+  })
+}
 
 
 function logout() {
@@ -101,7 +119,7 @@ function logout() {
   const auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut()
   .then( () => {
-    console.log('User signed out.');
+    showSuccessToastMessage('Goodbye! See you next time')
   })
   .catch(err =>{
       console.log(err.responseJSON.errors)
@@ -142,6 +160,232 @@ function showErrorToastMessage(message) {
   })
 }
 
+function fetchTodo(){
+  const unfinished = $('#unfinished')
+  const finished = $('#finished')
+
+  $.ajax({
+    url: `${baseUrl}/todos`,
+    method: 'GET',
+    headers:{
+      token: localStorage.token
+    }
+  })
+
+  .done(data => {
+    console.log(data)
+    unfinished.empty()
+    finished.empty()
+    getWeather()
+    $('#container-todo').empty()
+    data.forEach(todo => {
+      if(todo.status === 'not finished'){
+      unfinished.append(`
+      <li class="media bg-white rounded p-4 bg-light shadow mt-4" style="width: 100%; height: 100%; border-radius: 40px;">
+        <div class="media-body p-1">
+        <button onclick="deleteToDo(${todo.id})" type="button" class="close float-right" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+      </button>
+          <h5 class="mt-0 mb-0">${todo.title}</h5>
+          <br>
+          <span class="text-muted">${todo.description}</span> 
+          <br><br>
+          <small>Due: ${new Date(todo.due_date).toDateString()}</small>
+      <br><br>
+      <button onclick="toEditPage(${todo.id}, '${todo.title}', '${todo.description}', '${todo.status}', '${todo.due_date}')" class="btn btn-primary mr-2"><i class="fas fa-edit"></i> Update</button>
+      <button onclick="updateStatus(${todo.id}, 'finished')" class="btn btn-success"><i class="fas fa-check-circle"></i> Finished</button>
+      </li>`)
+
+    } else if (todo.status === 'finished') {
+      finished.append(`
+      <li class="media bg-white rounded p-4 bg-light shadow mt-4">
+      <div class="media-body p-1">
+      <button onclick="deleteToDo(${todo.id})" type="button" class="close float-right" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+    </button>
+        <h5 class="mt-0 mb-0">${todo.title}</h5>
+        <br>
+        <span class="text-muted">${todo.description}</span> 
+        <br><br>
+        <small>Due: ${new Date(todo.due_date).toDateString()}</small>
+    <br><br>
+    <button onclick=" "toEditPage(${todo.id}, '${todo.title}', '${todo.description}', '${todo.status}', '${todo.due_date}')" class="btn btn-primary mr-2"><i class="fas fa-edit"></i> Update</button>
+    <button onclick="updateStatus(${todo.id}, 'not finished')" class="btn btn-success"><i class="fas fa-check-circle"></i> Finished</button>
+    </li>`)
+    }
+  })
+})
+  .fail(err => {
+    console.log(err.responseJSON.errors, '<<< error login')
+    showErrorToastMessage(err.responseJSON.errors.join(', '))
+  })
+  .always(() => {
+    $("#login-email").val('');
+    $("#login-password").val('');
+  })
+}
+
+//ADD PAGE To Do
+
+function toAddPage() {
+  $('#add-page').show()
+  $('#home-page').hide()
+  hideEditForm()
+
+}
+
+//ADD To Do
+
+function addTask(event){
+  event.preventDefault()
+  const title = $('#add-title').val()
+  const description = $('#add-description').val()
+  const due_date = $('#add-due').val()
+  const status = 'not finished'
+
+  $.ajax({
+    url: `${baseUrl}/todos/`,
+    method:'POST',
+    headers: {
+      token:localStorage.token
+    },
+    data: {
+      title,
+      description,
+      due_date,
+      status
+    }
+  })
+  .done(() => {
+    fetchTodo()
+    $('#home-page').show()
+    $('#add-page').hide()
+    showSuccessMessage('Task has been created')
+  })
+  .fail(err => {
+    console.log(err.responseJSON.errors)
+  })
+  .always (() => {
+    $('#add-title').val('')
+    $('#add-description').val('')
+    $('#add-due').val('')
+  })
+}
+
+//DELETE To Do
+
+function deleteToDo(id) {
+  Swal.fire({
+    title: 'Oops! Are you sure?',
+    text: "You won't be able to see this task again!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete!'
+  })
+  .then((result) => {
+    if (result.isConfirmed) {
+      $.ajax({
+        url: `${baseUrl}/todos/${id}`,
+        method: "delete",
+        headers: {
+          token: localStorage.token 
+        },
+        data: {
+          status
+        }
+      })
+      .done(data => {
+        Swal.fire(
+          'Deleted!',
+          'Successfully deleted!',
+          'success'
+        )
+        fetchTodo()
+      })
+      .fail(err => {
+        showErrorToastMessage(err.responseJSON.errors.join('\n'))
+      })
+    }
+  })  
+}
+
+// Update Status
+
+function updateStatus(id, status) {
+  $.ajax({
+    url: `${baseUrl}/todos/${id}`,
+    method: "patch",
+    headers: {
+      token: localStorage.token 
+    },
+    data: {
+      status
+    }
+  })
+  .done(data => {
+    showSuccessMessage('Successfully Updated!')
+    fetchTodo()
+  })
+  .fail(err => {
+    showErrorToastMessage(err.responseJSON.errors.join('\n'))
+  })
+}
+
+//Edit Form
+
+function toEditPage(id, title, description, status, due_date) {
+  globalTodoId = id
+  $('#edit-title').val(title)
+  $('#edit-description').val(description)
+  $('#edit-due').val(new Date(due_date).toISOString().slice(0,10))
+  $('#edit-status').val(status)
+  $('#home-page').hide()
+  $('#edit-page').show()
+  
+  
+}
+
+function hideEditForm(event) {
+  if (event) event.preventDefault()
+  $('#edit-page').hide()
+  $('#add-page').show()
+}
+
+// Update
+function updateTodo(event) {
+  event.preventDefault()
+  const title = $('#edit-title').val()
+  const description = $('#edit-description').val()
+  const due_date = $('#edit-due').val()
+  const status = $('#edit-status').val()
+  $.ajax({
+    url: `${baseUrl}/todos/${id}`,
+    method: "put",
+    headers: {
+      token: localStorage.token 
+    },
+    data: {
+      title,
+      description,
+      due_date,
+      status
+    }
+  })
+  .done(data => {
+    $('#edit-title').val('')
+    $('#edit-description').val('')
+    $('#edit-due').val('')
+    $('#edt-status').val('')
+    showSuccessMessage('Woohoo! Successfully Updated!')
+    hideEditForm()
+    fetchTodo()
+  })
+  .fail(err => {
+    showErrorToastMessage(err.responseJSON.errors.join('\n'))
+  })
+}
 
 
 
