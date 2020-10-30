79536a50c86f1f244522b5899d811b08fa579f24
delviawp <delviawp@gmail.com>
2020-10-30 22:51:24 +0800

    register login server + client done

--- client/index.js
+++ client/index.js
@@ -3,19 +3,61 @@ const SERVER = "http://localhost:3000"
 $(document).ready(function () {
     const token = localStorage.getItem("token")
     console.log(token)
+    
     if(token) {
         $("#content").show()
-        $("#landing").hide()
+        $("#login-page").hide()
+        $("#btn-logout").show()
+        $("#register-page").hide()
         showTodos()
     } else {
         $("#content").hide()
-        $("#landing").show() 
+        $("#login-page").show() 
+        $("#register-page").hide()
+        $("#btn-logout").hide()
     }
-
     $("#btn-logout").on("click", function () {
         logout()
     })
 })
+
+function registerPage() {
+    $("#login-page").hide()
+    $("#register-page").show()
+    register()
+}
+
+function loginPage() {
+    $("#login-page").show()
+    $("#register-page").hide()
+    login(e)
+}
+
+function register() {
+    //e.preventDefault()
+    $("#login-page").hide()
+    $("#register-page").show()
+    //console.log('register')
+    const email = $("#email-reg").val()
+    const password = $("#password-reg").val()
+
+    $.ajax({
+        method: "POST",
+        url: SERVER + "/register",
+        data: {
+            email,
+            password,
+        },
+    }).done(response => {
+        $("#content").hide()
+        $("#btn-logout").hide()
+        $("#login-page").show()
+        $("#register-page").hide()
+    })
+    .fail(err => {
+        console.log(err)
+    })
+}
 
 function login(e) {
     e.preventDefault()
@@ -34,19 +76,22 @@ function login(e) {
     }).done(response => {
         const token = response.tokenAcces
         localStorage.setItem("token", token)
-        $("#landing").hide()
+        $("#login-page").hide()
+        $("#register-page").hide()
         $("#content").show()
+        $("#btn-logout").show()
         $("#email").val("")
         $("#password").val("")
-        //showTodos()
+        showTodos()
     }).fail(err => {
         console.log(err)
     })
 }
 
 function logout() {
-    $("#landing").show()
+    $("#login-page").show()
     $("#content").hide()
+    $("#btn-logout").hide()
     localStorage.removeItem('token')
 }
 
