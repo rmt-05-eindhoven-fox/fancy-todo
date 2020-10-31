6c1655c922ec98396fd80bd506ed1e3d91bfc88f
bobbyseptianto <bobbyseptianto@gmail.com>
2020-11-01 02:44:55 +0700

    mau coba deploy

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const router = require('./routers/router');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 const cors = require('cors');
 const errorHandler = require("./middlewares/errorHandler");
 
@@ -13,6 +13,6 @@ app.use(express.json());
 app.use(router);
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
 });
