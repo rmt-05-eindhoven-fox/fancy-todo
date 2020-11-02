7d7751b9d430646fcff7511c2984149f38a3f9d5
0x67 <gabri.putra@gmail.com>
2020-11-03 02:27:30 +0700

    Testing DiscordBot daily reminder

--- server/bots/bot.js
+++ server/bots/bot.js
@@ -68,29 +68,29 @@ client.on('message', (msg) => {
 const channelId = "772878233439830066"
 cron.schedule('* * * * *', function () {
    let channel = client.channels.cache.get(channelId)
-   let messageEmbed = new bot.MessageEmbed()
-      .setColor('#403b3b')
-      .setTitle('**This is automated daily reminder')
-      .setAuthor('RemindMeBot by 0x67', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRd2wriHCNJBK_7BBKzlJc6wQtVxR92SSASHQ&usqp=CAU')
-      .setDescription('You can do `!<command>` to run a command, or `!help` to view all available commands.')
-      .setThumbnail('https://cdn.iconscout.com/icon/free/png-256/reminder-19-461743.png')
-      .addFields({
-         name: 'list',
-         value: 'View all your not completed Todos.'
-      }, {
-         name: 'mark',
-         value: 'Mark your Todo as done. Example: `!mark 1`'
-      }, {
-         name: 'create',
-         value: '[title] [description] [due_date]'
-      }, {
-         name: 'remindMe',
-         value: 'Automatically remind you 1 day before your Todo deadline. Currently in development.'
-      }, )
-      .setTimestamp()
-      .setFooter(`You're amazing!`, 'https://cdn.iconscout.com/icon/free/png-256/reminder-19-461743.png');
-   // console.log(('cron daily jalan'));
-   channel.send(messageEmbed)
+   // let messageEmbed = new bot.MessageEmbed()
+   //    .setColor('#403b3b')
+   //    .setTitle('**This is automated daily reminder')
+   //    .setAuthor('RemindMeBot by 0x67', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRd2wriHCNJBK_7BBKzlJc6wQtVxR92SSASHQ&usqp=CAU')
+   //    .setDescription('You can do `!<command>` to run a command, or `!help` to view all available commands.')
+   //    .setThumbnail('https://cdn.iconscout.com/icon/free/png-256/reminder-19-461743.png')
+   //    .addFields({
+   //       name: 'list',
+   //       value: 'View all your not completed Todos.'
+   //    }, {
+   //       name: 'mark',
+   //       value: 'Mark your Todo as done. Example: `!mark 1`'
+   //    }, {
+   //       name: 'create',
+   //       value: '[title] [description] [due_date]'
+   //    }, {
+   //       name: 'remindMe',
+   //       value: 'Automatically remind you 1 day before your Todo deadline. Currently in development.'
+   //    }, )
+   //    .setTimestamp()
+   //    .setFooter(`You're amazing!`, 'https://cdn.iconscout.com/icon/free/png-256/reminder-19-461743.png');
+   channel.send('cron daily jalan')
+   // channel.send(messageEmbed)
 });
 
 // Bot login using token
