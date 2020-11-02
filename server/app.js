c0b89c9a7619c3c7c8422e0c81d248874d3a153c
dewijoanne <44321548+dewijoanne@users.noreply.github.com>
2020-11-02 13:40:29 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,14 +1,17 @@
 'use strict'
-require('dotenv').config()
+  require('dotenv').config()
+
 const express = require('express')
 const router = require('./routes/index')
 const errorHandler = require('./middlewares/errorHandler')
+const cors = require('cors')
 
 
 const app = express()
 const PORT = process.env.PORT || 3050
 
 
+app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(router)
