a5fb8c458f2549c7070e7ddd18397a8bfd7e45e4
dewijoanne <44321548+dewijoanne@users.noreply.github.com>
2020-11-02 12:07:50 +0700

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,7 +10,7 @@ const WeatherRoutes = require('./WeatherRoute')
 router.get('/', HomeController.home)
 router.post('/register', HomeController.register)
 router.post('/login', HomeController.login)
-
+router.post('/googleLogin', HomeController.googleLogin)
 
 router.use('/todos', todoRouter)
 router.use('/weathers',WeatherRoutes)
